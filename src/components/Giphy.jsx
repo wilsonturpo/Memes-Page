8ea8081d08362aca51273635e046b9d80dff216d
wilsonturpo <wilson.turpo@tecsup.edu.pe>
2021-11-27 12:13:20 -0500

    memes finished

---
+++ src/components/Giphy.jsx
@@ -0,0 +1,119 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import Loader from "./Loader";
+import Paginate from "./Paginate";
+
+const Giphy = () => {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(25);
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
+  //page 1 item 1 - item 25
+  //page 2 item 26 - item 50
+  //page 3 item 51 - item 75
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const results = await axios("https://api.giphy.com/v1/gifs/trending", {
+          params: {
+            api_key: "O6t7nZUwRDBqVQsa1TAIcs78CfRJ0vs3",
+            limit: 100
+          }
+        });
+
+        console.log(results);
+        setData(results.data.data);
+      } catch (err) {
+        setIsError(true);
+        setTimeout(() => setIsError(false), 4000);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  const renderGifs = () => {
+    if (isLoading) {
+      return <Loader />;
+    }
+    return currentItems.map(el => {
+      return (
+        <div key={el.id} className="gif">
+          <img src={el.images.fixed_height.url} />
+        </div>
+      );
+    });
+  };
+  const renderError = () => {
+    if (isError) {
+      return (
+        <div
+          className="alert alert-danger alert-dismissible fade show"
+          role="alert"
+        >
+          Unable to get Gifs, please try again in a few minutes
+        </div>
+      );
+    }
+  };
+
+  const handleSearchChange = event => {
+    setSearch(event.target.value);
+  };
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    setIsError(false);
+    setIsLoading(true);
+
+    try {
+      const results = await axios("https://4api.giphy.com/v1/gifs/search", {
+        params: {
+          api_key: "O6t7nZUwRDBqVQsa1TAIcs78CfRJ0vs3",
+          q: search,
+          limit: 100
+        }
+      });
+      setData(results.data.data);
+    } catch (err) {
+      setIsError(true);
+      setTimeout(() => setIsError(false), 4000);
+    }
+
+    setIsLoading(false);
+  };
+
+  const pageSelected = pageNumber => {
+    setCurrentPage(pageNumber);
+  };
+
+  return (
+    <div className="m-2">
+      {renderError()}
+        <a href="https://cai.tools.sap/kaarstthenancco/robot-bromas/monitor/usage-metrics" target="_blank" className="btn btn-primary">Ir a métricas</a>
+      <Paginate
+        pageSelected={pageSelected}
+        currentPage={currentPage}
+        itemsPerPage={itemsPerPage}
+        totalItems={data.length}
+      />
+      <div className="container gifs">{renderGifs()}</div>
+    </div>
+  );
+};
+
+export default Giphy;
